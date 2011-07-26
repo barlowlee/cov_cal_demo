2faab05ba2a5b1e4959dd4f3f6bc8617d963c8d2
Jonathan Lee <jlee@ctgla.org>
2011-07-25 21:54:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CovCalDemo::Application.config.secret_token = '75f69ba073474e25ad6a1b8e789d9c2b3a1768d2c2fec9e7e345f3f243bf81e82865223c15193d738cfa026d5f89dae357be042a7bf68b8be9ccce659c5c1716'
